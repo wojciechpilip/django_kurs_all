adc11db4dc5a27c31bdd240ce2edacdc87fda3da
Rafał <korzniewski@gmail.com>
2018-12-18 12:35:12 +0100

    Fixtures

--- bocian/blog/tests.py
+++ bocian/blog/tests.py
@@ -5,10 +5,15 @@ from blog.models import Wpis
 
 
 class TestBlogRoutes(TestCase):
-
+    fixtures = ['blog']
     def test_main_page_of_blog_exists(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
+
+    def test_czy_mozna_obejrzec_dowolny_wpis(self):
+        response = self.client.get('/blog/wpisy/2')
+        self.assertContains(response, '<h1>Drugi wpis</h1>')
+        self.assertContains(response, '<div class="tresc_wpisu">Zabawa była przednia</div>')
 
 
 class TestWpisModel(TestCase):
